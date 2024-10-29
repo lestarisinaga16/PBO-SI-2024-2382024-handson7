2dd624195d55787346c0940292dbb546726ab9af
oselviajuliana <113408146+oselviajuliana@users.noreply.github.com>
2024-10-29 18:07:55 +0700

    handson7

--- Polimorfisme/Lingkaran.java
+++ Polimorfisme/Lingkaran.java
@@ -1,5 +1,19 @@
 package Polimorfisme;
 
-public class Lingkaran {
+public class Lingkaran extends BangunDatar {
+    private double jariJari;
 
+    public Lingkaran(final double jariJari) {
+        this.jariJari = jariJari;
+    }
+
+    @Override
+    public double luas() {
+        return Math.PI * jariJari * jariJari;
+    }
+
+    @Override
+    public double keliling() {
+        return 2 * Math.PI * jariJari;
+    }
 }
--- Polimorfisme/SegitigaSamaSisi.java
+++ Polimorfisme/SegitigaSamaSisi.java
@@ -1,4 +1,19 @@
 package Polimorfisme;
 
-public class SegitigaSamaSisi {
+public class SegitigaSamaSisi extends BangunDatar {
+    private double sisi;
+
+    public SegitigaSamaSisi(final double sisi) {
+        this.sisi = sisi;
+    }
+
+    @Override
+    public double luas() {
+        return (Math.sqrt(3) / 4) * sisi * sisi;
+    }
+
+    @Override
+    public double keliling() {
+        return 3 * sisi;
+    }
 }
