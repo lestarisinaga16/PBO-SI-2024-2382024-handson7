16fba027bda5284b255fa31a1c117313a20ed689
oselviajuliana <113408146+oselviajuliana@users.noreply.github.com>
2024-10-29 10:21:26 +0700

    handson7

--- Polimorfisme/Persegi.java
+++ Polimorfisme/Persegi.java
@@ -1,17 +1,16 @@
 package Polimorfisme;
 
-public class Persegi extends BangunDatar {
-    private String sisi;
+public class Persegi extends BangunDatar{
+    private double sisi;
 
-
-    public Persegi(final double sisi) {
+    public Persegi(final double sisi){
         this.sisi = sisi;
     }
     @Override
     public double luas(){
         return sisi * sisi;
-
     }
+
     @Override
     public double keliling(){
         return 4 * sisi;
